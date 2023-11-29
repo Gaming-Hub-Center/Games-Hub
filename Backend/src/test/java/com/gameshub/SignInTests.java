604c2a1f0c729654dab6dd1fd8196fb059dbd5fe
Ehab Yasser <eng.ehabyasser@gmail.com>
2023-11-29 06:16:51 +0200

    trying automated test

--- Backend/src/test/java/com/gameshub/SignInTests.java
+++ Backend/src/test/java/com/gameshub/SignInTests.java
@@ -1,50 +1,55 @@
 package com.gameshub;
 
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import com.fasterxml.jackson.databind.*;
-import com.gameshub.Controller.*;
+import com.gameshub.Controller.AuthController;
 import com.gameshub.Model.Users.DAOs.BuyerDAO;
 import com.gameshub.Model.Users.DAOs.SellerDAO;
-import com.gameshub.Repository.*;
-import org.junit.jupiter.api.*;
-import org.mockito.*;
-import org.springframework.http.*;
-import org.springframework.test.web.servlet.*;
-import org.springframework.test.web.servlet.request.*;
-import org.springframework.test.web.servlet.setup.*;
+import com.gameshub.Repository.BuyerRepository;
+import com.gameshub.Repository.SellerRepository;
+import com.gameshub.Service.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.time.*;
-import java.util.*;
+import java.time.LocalDate;
+import java.util.Optional;
 
+@WebMvcTest(AuthController.class)
 public class SignInTests {
 
+    @Autowired
     private MockMvc mockMvc;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Mock
+    @MockBean
     private BuyerRepository buyerRepository;
-    @Mock
+
+    @MockBean
     private SellerRepository sellerRepository;
 
-    @InjectMocks
-    private AuthController authController;
+    @MockBean
+    private BuyerDetailsService buyerDetailsService;
 
-    BuyerDAO buyerDAO1 = new BuyerDAO(1, "John Doe", "john.doe@example.com", "password123", "+1234567890", "123 Elm Street", 3000);
-    BuyerDAO buyerDAO2 = new BuyerDAO(2, "Jane Smith", "jane.smith@example.com", "mypassword", "+9876543210", "456 Oak Avenue", 4000);
-    SellerDAO sellerDAO1 = new SellerDAO(1, "Alice Blue", "alice.blue@example.com", "alicepass", "+1029384756", "101 Red Street", 10000, "ID12345A", LocalDate.of(2023, 1, 1), "Description about Alice", "123456789A");
-    SellerDAO sellerDAO2 = new SellerDAO(2, "Bob Green", "bob.green@example.com", "bobpassword", "+5647382910", "202 Green Lane", 15000, "ID67890B", LocalDate.of(2023, 2, 2), "Description about Bob", "987654321B");
+    @MockBean
+    private SellerDetailsService sellerDetailsService;
+
+    private BuyerDAO buyerDAO1 = new BuyerDAO(1, "John Doe", "john.doe@example.com", "password123", "+1234567890", "123 Elm Street", 3000);
+    private BuyerDAO buyerDAO2 = new BuyerDAO(2, "Jane Smith", "jane.smith@example.com", "mypassword", "+9876543210", "456 Oak Avenue", 4000);
+    private SellerDAO sellerDAO1 = new SellerDAO(1, "Alice Blue", "alice.blue@example.com", "alicepass", "+1029384756", "101 Red Street", 10000, "ID12345A", LocalDate.of(2023, 1, 1), "Description about Alice", "123456789A");
+    private SellerDAO sellerDAO2 = new SellerDAO(2, "Bob Green", "bob.green@example.com", "bobpassword", "+5647382910", "202 Green Lane", 15000, "ID67890B", LocalDate.of(2023, 2, 2), "Description about Bob", "987654321B");
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
-
-        when(buyerRepository.findByEmail("john.doe@example.com")).thenReturn(Optional.of(buyerDAO1));
-        when(buyerRepository.findByEmail("jane.smith@example.com")).thenReturn(Optional.of(buyerDAO2));
-        when(sellerRepository.findByEmail("alice.blue@example.com")).thenReturn(Optional.of(sellerDAO1));
-        when(sellerRepository.findByEmail("bob.green@example.com")).thenReturn(Optional.of(sellerDAO2));
+        Mockito.when(buyerRepository.findByEmail("john.doe@example.com")).thenReturn(Optional.of(buyerDAO1));
+        Mockito.when(buyerRepository.findByEmail("jane.smith@example.com")).thenReturn(Optional.of(buyerDAO2));
+        Mockito.when(sellerRepository.findByEmail("alice.blue@example.com")).thenReturn(Optional.of(sellerDAO1));
+        Mockito.when(sellerRepository.findByEmail("bob.green@example.com")).thenReturn(Optional.of(sellerDAO2));
     }
 
     @Test
@@ -52,10 +57,10 @@ public class SignInTests {
         String signInRequestJson = "{\"email\":\"john.doe@example.com\",\"password\":\"password123\"}";
 
         mockMvc.perform(MockMvcRequestBuilders
-            .post("/auth/signin")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(signInRequestJson))
-            .andExpect(status().isOk());
+                        .post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signInRequestJson))
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -63,10 +68,10 @@ public class SignInTests {
         String signInRequestJson = "{\"email\":\"invalid_email@example.com\",\"password\":\"pass\"}";
 
         mockMvc.perform(MockMvcRequestBuilders
-            .post("/auth/signin")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(signInRequestJson))
-            .andExpect(status().isNotFound());
+                        .post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signInRequestJson))
+                .andExpect(status().isNotFound());
     }
 
     @Test
@@ -74,10 +79,10 @@ public class SignInTests {
         String signInRequestJson = "{\"email\":\"john.doe@example.com\",\"password\":\"invalid_pass\"}";
 
         mockMvc.perform(MockMvcRequestBuilders
-            .post("/auth/signin")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(signInRequestJson))
-            .andExpect(status().isUnauthorized());
+                        .post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signInRequestJson))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
@@ -85,10 +90,9 @@ public class SignInTests {
         String signInRequestJson = "{\"email\":\"john.doe@example\",\"password\":\"password123\"}";
 
         mockMvc.perform(MockMvcRequestBuilders
-            .post("/auth/signin")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(signInRequestJson))
-            .andExpect(status().isBadRequest());
+                        .post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signInRequestJson))
+                .andExpect(status().isBadRequest());
     }
-
 }
