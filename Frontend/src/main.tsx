13fd051280998e541ea0c55eba5938732bf27b33
Rowaina <96254289+rowaina2025@users.noreply.github.com>
2023-11-29 04:21:45 +0200

    Revert "Gh 150 google o auth sign up"

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SignIn from "./SignIn.tsx"
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SignIn />
+    <App />
   </React.StrictMode>,
 )
