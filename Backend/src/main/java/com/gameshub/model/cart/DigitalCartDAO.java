7c4acde78be1a2a3e520d6229b71b0e8841c568a
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-12-11 14:08:51 +0200

    added Setters and Getters
    added Setters and Getters, to physical/digital cart DAO
    to return its elements properly through the controller

--- Backend/src/main/java/com/gameshub/model/cart/DigitalCartDAO.java
+++ Backend/src/main/java/com/gameshub/model/cart/DigitalCartDAO.java
@@ -3,8 +3,12 @@ package com.gameshub.model.cart;
 import com.gameshub.model.product.DigitalProductDAO;
 import com.gameshub.model.user.BuyerDAO;
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "digital_cart")
 @NoArgsConstructor
--- Backend/src/main/java/com/gameshub/model/cart/PhysicalCartDAO.java
+++ Backend/src/main/java/com/gameshub/model/cart/PhysicalCartDAO.java
@@ -4,8 +4,12 @@ package com.gameshub.model.cart;
 import com.gameshub.model.product.PhysicalProductDAO;
 import com.gameshub.model.user.BuyerDAO;
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "physical_cart")
 @NoArgsConstructor
