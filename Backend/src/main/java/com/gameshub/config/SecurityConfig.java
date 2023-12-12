1b85611c8ed984141081ac44adead216355dea6c
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-12-12 04:08:25 +0200

    Tests
    added Tests for CartService, CartController(not Complete)

--- Backend/src/main/java/com/gameshub/config/SecurityConfig.java
+++ Backend/src/main/java/com/gameshub/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/cart/**")).permitAll()
+                        //.requestMatchers(new AntPathRequestMatcher("/cart/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/oauth2/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/registration/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
