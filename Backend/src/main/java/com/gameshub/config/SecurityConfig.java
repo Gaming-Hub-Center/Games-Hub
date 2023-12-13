e42cfaed5874d97d31b696369dea387dd38f5b8e
Rowaina <rowainaabdelnasser@gmail.com>
2023-12-13 20:33:25 +0200

    change permitAll

--- Backend/src/main/java/com/gameshub/config/SecurityConfig.java
+++ Backend/src/main/java/com/gameshub/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/registration/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/products/approve/**")).permitAll()
+//                        .requestMatchers(new AntPathRequestMatcher("/product-request/create/**")).permitAll()
                         .anyRequest().authenticated());
 //                .oauth2Login(Customizer.withDefaults());
 
