055be777a8f6322214718f144266ddd1a5bd8c31
Marwan Yasser <96551919+Maro1123@users.noreply.github.com>
2023-12-14 08:12:56 +0200

    Fix failing tests of orders

--- Backend/src/test/java/com/gameshub/DigitalOrderTests.java
+++ Backend/src/test/java/com/gameshub/DigitalOrderTests.java
@@ -45,56 +45,56 @@ public class DigitalOrderTests {
     @Autowired
     DigitalProductRepository digitalProductRepository;
 
-//    @BeforeEach
-//    public void setup() {
-//        digitalOrderItemRepository.deleteAll();
-//        orderRepository.deleteAll();
-//        digitalCartRepository.deleteAll();
-//        digitalProductRepository.deleteAll();
-//        sellerRepository.deleteAll();
-//        buyerRepository.deleteAll();
-//
-//        orderRepository.resetAutoIncrement();
-//        digitalProductRepository.resetAutoIncrement();
-//        sellerRepository.resetAutoIncrement();
-//        buyerRepository.resetAutoIncrement();
-//
-//        BuyerDAO buyerDAO1 = new BuyerDAO("John Doe", "john.doe@example.com", "$2a$10$YJGLrNDJ0F.mE2E6IFWnDeDrkKlvQ3FuSYaOiUieGjTMkraZJoRBG", "+1234567890", "123 Elm Street", 1000);
-//        BuyerDAO buyerDAO2 = new BuyerDAO("Jane Smith", "+9876543210", "jane.smith@example.com", "456 Oak Avenue", "$2a$10$GMRwzM/Li/rDwHDw4RNbBeQFRHEcwVSEBZ18D4NkUo5BiHd/oawP6", 4000);
-//
-//        SellerDAO sellerDAO1 = new SellerDAO("Alice Blue", "alice.blue@example.com", "$2a$10$HaID.XdQm../yady9rA2k.EoY4oiL/In32c/cLRa3DWyW/Nn6DXcG", "+1029384756", "101 Red Street", 10000, "ID12345A", LocalDate.parse("2023-01-01"), "Description about Alice", "123456789A");
-//        SellerDAO sellerDAO2 = new SellerDAO("Bob Green", "+5647382910", "bob.green@example.com", "$2a$10$bM/Om9b6r/7qklzXJCCAauNotDkmKEsD2W.BA32PvOw5nIcOsRc3q", "202 Green Lane", 15000, "ID67890B", LocalDate.parse("2023-02-02"), "Description about Bob", "987654321B");
-//
-//        DigitalProductDAO digitalProductDAO1 = new DigitalProductDAO("Digital Game Bundle", 300.00f, "Get the latest digital game bundle with multiple titles.", 1, 10, "Games", "DG001", LocalDate.parse("2023-12-08"));
-//        DigitalProductDAO digitalProductDAO2 = new DigitalProductDAO("Virtual Reality Gaming Experience", 200.00f, "Immersive VR gaming with a high-resolution headset.", 1, 15, "VR Games", "VR001", LocalDate.parse("2023-12-08"));
-//        DigitalProductDAO digitalProductDAO3 = new DigitalProductDAO("Wireless Gaming Controller Pack", 60.00f, "Bundle of ergonomic wireless controllers for various gaming platforms.", 2, 25, "Accessories", "WG001", LocalDate.parse("2023-12-08"));
-//        DigitalProductDAO digitalProductDAO4 = new DigitalProductDAO("High-Performance Gaming Laptop", 1000.00f, "Top-notch gaming laptop for an exceptional gaming experience.", 2, 5, "Computers", "HL001", LocalDate.parse("2023-12-08"));
-//        DigitalProductDAO digitalProductDAO5 = new DigitalProductDAO("Epic Quests Strategy Game", 50.00f, "Embark on epic quests with our latest strategy game.", 2, 2, "Games", "EQ001", LocalDate.parse("2023-12-08"));
-//
-//        DigitalCartDAO digitalCartDAO1 = new DigitalCartDAO(1, 1, 5);
-//        DigitalCartDAO digitalCartDAO2 = new DigitalCartDAO(1, 3, 2);
-//        DigitalCartDAO digitalCartDAO3 = new DigitalCartDAO(1, 5, 2);
-//        DigitalCartDAO digitalCartDAO4 = new DigitalCartDAO(2, 2, 2);
-//        DigitalCartDAO digitalCartDAO5 = new DigitalCartDAO(2, 4, 7);
-//
-//        buyerRepository.save(buyerDAO1);
-//        buyerRepository.save(buyerDAO2);
-//
-//        sellerRepository.save(sellerDAO1);
-//        sellerRepository.save(sellerDAO2);
-//
-//        digitalProductRepository.save(digitalProductDAO1);
-//        digitalProductRepository.save(digitalProductDAO2);
-//        digitalProductRepository.save(digitalProductDAO3);
-//        digitalProductRepository.save(digitalProductDAO4);
-//        digitalProductRepository.save(digitalProductDAO5);
-//
-//        digitalCartRepository.save(digitalCartDAO1);
-//        digitalCartRepository.save(digitalCartDAO2);
-//        digitalCartRepository.save(digitalCartDAO3);
-//        digitalCartRepository.save(digitalCartDAO4);
-//        digitalCartRepository.save(digitalCartDAO5);
-//    }
+    @BeforeEach
+    public void setup() {
+        digitalOrderItemRepository.deleteAll();
+        orderRepository.deleteAll();
+        digitalCartRepository.deleteAll();
+        digitalProductRepository.deleteAll();
+        sellerRepository.deleteAll();
+        buyerRepository.deleteAll();
+
+        orderRepository.resetAutoIncrement();
+        digitalProductRepository.resetAutoIncrement();
+        sellerRepository.resetAutoIncrement();
+        buyerRepository.resetAutoIncrement();
+
+        BuyerDAO buyerDAO1 = new BuyerDAO("John Doe", "john.doe@example.com", "$2a$10$YJGLrNDJ0F.mE2E6IFWnDeDrkKlvQ3FuSYaOiUieGjTMkraZJoRBG", "+1234567890", "123 Elm Street", 1000);
+        BuyerDAO buyerDAO2 = new BuyerDAO("Jane Smith", "+9876543210", "jane.smith@example.com", "456 Oak Avenue", "$2a$10$GMRwzM/Li/rDwHDw4RNbBeQFRHEcwVSEBZ18D4NkUo5BiHd/oawP6", 4000);
+
+        SellerDAO sellerDAO1 = new SellerDAO("Alice Blue", "alice.blue@example.com", "$2a$10$HaID.XdQm../yady9rA2k.EoY4oiL/In32c/cLRa3DWyW/Nn6DXcG", "+1029384756", "101 Red Street", 10000, "ID12345A", LocalDate.parse("2023-01-01"), "Description about Alice", "123456789A");
+        SellerDAO sellerDAO2 = new SellerDAO("Bob Green", "+5647382910", "bob.green@example.com", "$2a$10$bM/Om9b6r/7qklzXJCCAauNotDkmKEsD2W.BA32PvOw5nIcOsRc3q", "202 Green Lane", 15000, "ID67890B", LocalDate.parse("2023-02-02"), "Description about Bob", "987654321B");
+
+        DigitalProductDAO digitalProductDAO1 = new DigitalProductDAO("Digital Game Bundle", 300.00f, "Get the latest digital game bundle with multiple titles.", sellerDAO1, 10, "Games", "DG001", LocalDate.parse("2023-12-08"));
+        DigitalProductDAO digitalProductDAO2 = new DigitalProductDAO("Virtual Reality Gaming Experience", 200.00f, "Immersive VR gaming with a high-resolution headset.", sellerDAO1, 15, "VR Games", "VR001", LocalDate.parse("2023-12-08"));
+        DigitalProductDAO digitalProductDAO3 = new DigitalProductDAO("Wireless Gaming Controller Pack", 60.00f, "Bundle of ergonomic wireless controllers for various gaming platforms.", sellerDAO2, 25, "Accessories", "WG001", LocalDate.parse("2023-12-08"));
+        DigitalProductDAO digitalProductDAO4 = new DigitalProductDAO("High-Performance Gaming Laptop", 1000.00f, "Top-notch gaming laptop for an exceptional gaming experience.", sellerDAO2, 5, "Computers", "HL001", LocalDate.parse("2023-12-08"));
+        DigitalProductDAO digitalProductDAO5 = new DigitalProductDAO("Epic Quests Strategy Game", 50.00f, "Embark on epic quests with our latest strategy game.", sellerDAO2, 2, "Games", "EQ001", LocalDate.parse("2023-12-08"));
+
+        DigitalCartDAO digitalCartDAO1 = new DigitalCartDAO(1, 1, 5);
+        DigitalCartDAO digitalCartDAO2 = new DigitalCartDAO(1, 3, 2);
+        DigitalCartDAO digitalCartDAO3 = new DigitalCartDAO(1, 5, 2);
+        DigitalCartDAO digitalCartDAO4 = new DigitalCartDAO(2, 2, 2);
+        DigitalCartDAO digitalCartDAO5 = new DigitalCartDAO(2, 4, 7);
+
+        buyerRepository.save(buyerDAO1);
+        buyerRepository.save(buyerDAO2);
+
+        sellerRepository.save(sellerDAO1);
+        sellerRepository.save(sellerDAO2);
+
+        digitalProductRepository.save(digitalProductDAO1);
+        digitalProductRepository.save(digitalProductDAO2);
+        digitalProductRepository.save(digitalProductDAO3);
+        digitalProductRepository.save(digitalProductDAO4);
+        digitalProductRepository.save(digitalProductDAO5);
+
+        digitalCartRepository.save(digitalCartDAO1);
+        digitalCartRepository.save(digitalCartDAO2);
+        digitalCartRepository.save(digitalCartDAO3);
+        digitalCartRepository.save(digitalCartDAO4);
+        digitalCartRepository.save(digitalCartDAO5);
+    }
 
     @AfterEach
     public void finish() {
