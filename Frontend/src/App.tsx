f2f582db2708d7fd8cc389e0fc198b4de94394db
Islam0143 <sokoclash123@gmail.com>
2023-12-27 01:36:28 +0200

    UI not adjusted yet

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -19,6 +19,7 @@ import { SellerProductView } from "./Pages/Seller_Pages/ProductView/SellerProduc
 import { SellerProductEdit } from "./Pages/Seller_Pages/ProductEdit/SellerProductEdit";
 import {SellerView} from "./Pages/Admin/SellerView";
 import {BuyerView} from "./Pages/Admin/BuyerView";
+import {SellerCatalogView} from "./Pages/Admin/SellerCatalogView";
 
 
 function App() {
@@ -59,6 +60,7 @@ function App() {
         <Route path="/buyer/productview/:id" element={<ProductView />}></Route>
         <Route path="/admin/view/sellers" element={<SellerView />}></Route>
         <Route path="/admin/view/buyers" element={<BuyerView />}></Route>
+        <Route path="/admin/view/seller/products/:sellerId" element={<SellerCatalogView />}></Route>
       </Routes>
     </Container>
   );
--- Frontend/src/Pages/Admin/SellerView.tsx
+++ Frontend/src/Pages/Admin/SellerView.tsx
@@ -2,16 +2,18 @@ import {useEffect, useState} from "react";
 import { UserDTO } from "../../Controller/DTO/user/UserDTO";
 import { httpRequest } from "../../Controller/HttpProxy";
 import { Card, Container, Row, Col, Pagination, Button } from 'react-bootstrap';
+import {useNavigate} from "react-router-dom";
 
 
 export function SellerView() {
     const [sellersList, setSellersList] = useState<UserDTO[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 6;
+    const itemsPerPage = 8;
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const currentSellers = sellersList.slice(startIndex, endIndex);
     const totalPages = Math.ceil(sellersList.length / itemsPerPage);
+    const navigate = useNavigate();
 
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
@@ -40,6 +42,10 @@ export function SellerView() {
         }
     }
 
+    function goToViewSellerProducts(sellerId: number): void {
+        navigate(`/admin/view/seller/products/${sellerId}`);
+    }
+
     const renderPagination = () => {
         let items = [];
         for (let number = 1; number <= totalPages; number++) {
@@ -53,52 +59,57 @@ export function SellerView() {
     };
 
     return (
-        <div style={{ backgroundColor: '#121212', height: '100vh' }}>
-            <Container className="mt-4" style={{ backgroundColor: 'rgba(169, 169, 169, 0.8)', color: 'white' }}>
-                <Row>
-                    {currentSellers.map((seller, index) => (
-                        <Col key={index} md={4} className="mb-4">
-                            <Card style={{ backgroundColor: 'black', color: 'white' }}>
-                                <Card.Header as="h5" style={{ backgroundColor: '#733BC0', color: 'black' }}>{seller.name}</Card.Header>
-                                <Card.Body>
-                                    <Card.Text><strong>Email:</strong> {seller.email}</Card.Text>
-                                    <Card.Text><strong>Phone:</strong> {seller.phone}</Card.Text>
-                                    <Card.Text><strong>Address:</strong> {seller.address}</Card.Text>
-                                    <Card.Text><strong>balance:</strong> {seller.balance}</Card.Text>
-                                    <Card.Text><strong>nationalID:</strong> {seller.nationalID}</Card.Text>
-                                    <Card.Text><strong>dateJoined:</strong> {new Date(seller.dateJoined).toLocaleDateString('en-US')}</Card.Text>
-                                    <Card.Text><strong>sellerDescription:</strong> {seller.sellerDescription}</Card.Text>
-                                    <Card.Text><strong>vatRegistrationNumber:</strong> {seller.vatRegistrationNumber}</Card.Text>
+        <Container fluid style={{ backgroundColor: 'darkslateblue', color: 'white', height: '100vh', overflow: "hidden" }}>
+            <Row style={{width: '100%', height: '95.5vh', marginLeft:'170px'}}>
+                {currentSellers.map((seller, index) => (
+                    <Col key={index} md={3} style={{margin: '8px', width: '20%'}}  >
+                        <Card style={{ backgroundColor: 'black', color: 'white', height: '46vh', width: '100%' }}>
+                            <Card.Header as="h5" style={{ backgroundColor: '#733BC0', color: 'black', height: '4vh' }}>{seller.name}</Card.Header>
+                            <Card.Body style={{height: '30vh'}}>
+                                <Card.Text><strong>Email:</strong> {seller.email}</Card.Text>
+                                <Card.Text><strong>Phone:</strong> {seller.phone}</Card.Text>
+                                <Card.Text><strong>Address:</strong> {seller.address}</Card.Text>
+                                <Card.Text><strong>balance:</strong> {seller.balance}</Card.Text>
+                                <Card.Text><strong>nationalID:</strong> {seller.nationalID}</Card.Text>
+                                <Card.Text><strong>sellerDescription:</strong> {seller.sellerDescription}</Card.Text>
+                                <Card.Text><strong>vatRegistrationNumber:</strong> {seller.vatRegistrationNumber}</Card.Text>
+                                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between",}} >
                                     <Button
-                                        style={{backgroundColor: "#733BC0", color: "#f0f0f0", borderColor: "#733BC0", borderRadius: "5px", cursor: "pointer", transition: "background-color 0.3s",}}
+                                        style={{backgroundColor: "#733BC0", color: "#f0f0f0", borderColor: "#733BC0", borderRadius: "5px", cursor: "pointer", transition: "background-color 0.3s", height: '4vh'}}
                                         onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = "rgba(115,	59,	192 ,0.5)")}
                                         onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = "#733BC0")}
                                         onClick={() => deleteSeller(seller.id)}
                                     >
                                         Remove From System
                                     </Button>
-                                </Card.Body>
-                                <Card.Footer style={{ backgroundColor: 'white', color: 'black' }}>
-                                    <small className="text-muted">Joined: {new Date(seller.dateJoined).toLocaleDateString('en-US')}</small>
-                                </Card.Footer>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-                <Row>
-                    <Col>
-                        <div className="d-flex justify-content-center my-4">
-                            <Pagination style={{ justifyContent: 'center' }}>
-                                <Pagination.First onClick={() => handlePageChange(1)} disabled={currentPage === 1} />
-                                <Pagination.Prev onClick={() => handlePageChange(Math.max(1, currentPage - 1))} disabled={currentPage === 1} />
-                                {renderPagination()}
-                                <Pagination.Next onClick={() => handlePageChange(Math.min(totalPages, currentPage + 1))} disabled={currentPage === totalPages} />
-                                <Pagination.Last onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} />
-                            </Pagination>
-                        </div>
+                                    <Button
+                                        style={{backgroundColor: "#733BC0", color: "#f0f0f0", borderColor: "#733BC0", borderRadius: "5px", cursor: "pointer", transition: "background-color 0.3s", height: '4vh'}}
+                                        onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = "rgba(115,	59,	192 ,0.5)")}
+                                        onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = "#733BC0")}
+                                        onClick={() => goToViewSellerProducts(seller.id)}
+                                    >
+                                        View Products
+                                    </Button>
+                                </div>
+                            </Card.Body>
+                            <Card.Footer style={{ backgroundColor: 'white', color: 'black', height: '4vh' }}>
+                                <small className="text-muted">Joined: {new Date(seller.dateJoined).toLocaleDateString('en-US')}</small>
+                            </Card.Footer>
+                        </Card>
                     </Col>
-                </Row>
-            </Container>
-        </div>
+                ))}
+            </Row>
+            <Row>
+                <Col style={{ height: "8vh", display: "flex", justifyContent: "center",}}>
+                    <Pagination style={{ justifyContent: 'center' }}>
+                        <Pagination.First onClick={() => handlePageChange(1)} disabled={currentPage === 1} />
+                        <Pagination.Prev onClick={() => handlePageChange(Math.max(1, currentPage - 1))} disabled={currentPage === 1} />
+                        {renderPagination()}
+                        <Pagination.Next onClick={() => handlePageChange(Math.min(totalPages, currentPage + 1))} disabled={currentPage === totalPages} />
+                        <Pagination.Last onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} />
+                    </Pagination>
+                </Col>
+            </Row>
+        </Container>
     );
 }
