d1ea74e4f761ecf7def5cc559d014f363ba53f88
Islam0143 <sokoclash123@gmail.com>
2023-12-27 01:36:28 +0200

    UI not adjusted yet

--- Frontend/src/Pages/Admin/SellerCatalogView.tsx
+++ Frontend/src/Pages/Admin/SellerCatalogView.tsx
@@ -1,12 +1,14 @@
 import {useEffect, useState} from "react";
 import {ProductBriefDTO} from "../../Controller/DTO/ProductDTO/ProductBriefDTO";
 import {httpRequest} from "../../Controller/HttpProxy";
-import {Container} from "react-bootstrap";
+import {Card, Container} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import {useParams} from "react-router-dom";
 
 
 export function SellerCatalogView() {
+    const { sellerId } = useParams();
     const [physicalProducts, setPhysicalProducts] = useState<ProductBriefDTO[]>([]);
     const [digitalProducts, setDigitalProducts] = useState<ProductBriefDTO[]>([]);
 
@@ -22,36 +24,52 @@ export function SellerCatalogView() {
     }, []);
 
     const getAllPhysical = async() => {
-        const response = await httpRequest("GET", `/admin/view/physical/products/{sellerId}`);
+        const response = await httpRequest("GET", `/admin/view/physical/products/${sellerId}`);
         return response.data as ProductBriefDTO[]
     }
 
     const getAllDigital = async() => {
-        const response = await httpRequest("GET", `/admin/view/digital/products/{sellerId}`);
+        const response = await httpRequest("GET", `/admin/view/digital/products/${sellerId}`);
         return response.data as ProductBriefDTO[]
     }
 
 
 
 
-    return(
-        <Container>
-            <Col>
-                <Row>
-                    Physical Products
-                </Row>
-                <Row>
-
-                </Row>
-            </Col>
-            <Col>
-                <Row>
-                    Digital Products
-                </Row>
-                <Row>
-
-                </Row>
-            </Col>
+    return (
+        <Container fluid style={{ backgroundColor: 'darkslateblue', color: 'white', height: '100vh', overflow: "auto", padding: '20px' }}>
+            <Row>
+                <Col md={6}>
+                    <h3>Digital Products</h3>
+                    {digitalProducts.map((product, index) => (
+                        <Col key={index} md={4} className="mb-4">
+                            <Card style={{ backgroundColor: 'black' }}>
+                                <Card.Img variant="top" src={product.image} />
+                                <Card.Body>
+                                    <Card.Title>{product.title}</Card.Title>
+                                    <Card.Text>{product.description}</Card.Text>
+                                    <Card.Text>Price: ${product.price}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Col>
+                <Col md={6}>
+                    <h3>Physical Products</h3>
+                    {physicalProducts.map((product, index) => (
+                        <Col key={index} md={3} style={{margin: '8px', width: '20%'}}  >
+                            <Card className="mb-3" style={{ backgroundColor: 'black' }}>
+                                <Card.Img variant="top" src={product.image} />
+                                <Card.Body>
+                                    <Card.Title>{product.title}</Card.Title>
+                                    <Card.Text>{product.description}</Card.Text>
+                                    <Card.Text>Price: ${product.price}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Col>
+            </Row>
         </Container>
     );
 }
