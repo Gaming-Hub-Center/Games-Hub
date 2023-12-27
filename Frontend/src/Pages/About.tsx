cdead600f9ca5798dde2000a2e3b445fcae6064a
Rowaina <rowainaabdelnasser@gmail.com>
2023-12-27 23:09:25 +0200

    hopefully resolved the conflicts

--- Frontend/src/Pages/About.tsx
+++ Frontend/src/Pages/About.tsx
@@ -6,7 +6,7 @@ export function About() {
     <Container fluid style={{ height: "200vh", padding: 0 }}>
       <Row>
         <Col md={12}>
-          {/* <NavbarC ></NavbarC> // return ID */}
+          <NavbarC productType={undefined} updateProductCardPropsList={undefined}></NavbarC>
           <h1>About Page</h1>
           <p>Welcome to the about page. Here you can find out more about us.</p>
           <p>Welcome to the about page. Here you can find out more about us.</p>
